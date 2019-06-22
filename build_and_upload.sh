eac92726d1f80fe4920adddd2ceb415816fb8cf3
Sam Mohr <sammohr97@gmail.com>
2019-06-22 01:08:50 -0400

    fixed build script

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo build --release --target=x86_64-unknown-linux-musl && scp target/x86_64-unknown-linux-musl/release/grease_api mensgleeclub@gleeclub.gatech.edu:/cgi-bin/api
+cargo build --release --target=x86_64-unknown-linux-musl && scp target/x86_64-unknown-linux-musl/release/api mensgleeclub@gleeclub.gatech.edu:/cgi-bin/api
