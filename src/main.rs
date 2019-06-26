9da43279e7fd3a42c16ae76f3c6e174fcabc0551
Sam Mohr <sammohr97@gmail.com>
2019-06-26 03:08:46 -0400

    moved back from lib to binary

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
-extern crate grease_api;
+#![feature(custom_attribute)]
+#![feature(drain_filter)]
+#![feature(box_syntax)]
+#![feature(const_fn)]
+#![recursion_limit = "128"]
 
-use grease_api::routes::handle_request;
+extern crate base64;
+extern crate cgi;
+extern crate chrono;
+extern crate dotenv;
+extern crate glob;
+extern crate grease_derive;
+extern crate http;
+extern crate mysql;
+extern crate mysql_enum;
+extern crate pinto;
+extern crate regex;
+extern crate serde;
+extern crate serde_json;
+extern crate strum;
+extern crate strum_macros;
+extern crate url;
+// extern crate lettre;
+// extern crate lettre_email;
+#[cfg(test)]
+extern crate mocktopus;
+
+mod auth;
+mod db;
+mod error;
+mod extract;
+pub mod routes;
+mod util;
+
+use routes::handle_request;
 
 fn main() {
     dotenv::dotenv().ok();
