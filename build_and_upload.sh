34c3d42934cb5946cf71c4b872774e32ee6b2abd
Sam Mohr <sam@mohr.codes>
2021-06-02 19:35:41 -0400

    move crates into folders

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cargo build --release --target=x86_64-unknown-linux-musl && curl "https://gleeclub.gatech.edu/cgi-bin/admin_tools/upload_api" -H "token: $GREASE_TOKEN" -H "Content-Type: application/zip" --data-binary "@target/x86_64-unknown-linux-musl/release/grease_api"
+cargo build --release --target=x86_64-unknown-linux-musl && \
+    curl "https://gleeclub.gatech.edu/cgi-bin/admin_tools/upload_api" \
+    -H "token: $GREASE_TOKEN" -H "Content-Type: application/zip" \
+    --data-binary "@target/x86_64-unknown-linux-musl/release/grease_api"
