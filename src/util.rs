1100f236be951bcdde25b0cd3a3fcfcd81be6cad
Sam Mohr <sam.mohr@protonmail.com>
2022-07-02 00:19:49 -0700

    Fix Current Timezone Retrieval

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,18 @@
-use time::OffsetDateTime;
+use time::{OffsetDateTime, UtcOffset};
+use tz::{DateTime, TimeZone};
 
+/// Gets the current time.
+///
+/// More involved than a normal implementation, because getting the
+/// current timezone has proven difficult in the containerized
+/// environment we deploy in.
 pub fn current_time() -> OffsetDateTime {
-    OffsetDateTime::now_local().expect("Failed to get current timezone")
+    let timezone = TimeZone::local().expect("Failed to get current timezone");
+    let now = DateTime::now(timezone.as_ref()).expect("Failed to get current time");
+    let utc_offset = UtcOffset::from_whole_seconds(now.local_time_type().ut_offset())
+        .expect("Failed to build timezone object");
+
+    OffsetDateTime::from_unix_timestamp(now.unix_time())
+        .expect("Failed to convert between time constructs")
+        .to_offset(utc_offset)
 }
